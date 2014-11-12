6eb6222a29f693874a54a70758708b639830ce24
Mark Steele <zirrelstudios@gmail.com>
2014-11-12 17:55:18 +0000

    as

--- www/crypto.js
+++ www/crypto.js
@@ -1,18 +1,24 @@
+require('cordova/argscheck'),
+channel = require('cordova/channel'),	    
+utils = require('cordova/utils'),
+exec = require('cordova/exec'),
+cordova = require('cordova');
+	
+function Crypto(){
+};
+	
+Crypto.prototype.encrypt = function(successCallback, errorCallback, params) {
+	var plainText 	= params.text || '';
+	var returnTo 	= params.returnTo || function(){};
+	exec(successCallback, errorCallback, "Crypto", "encrypt", [{data: plainText}]);
+};
+	
+Crypto.prototype.decrypt = function(successCallback, errorCallback) {
+	exec(successCallback, errorCallback, "Crypto", "decrypt", [{data: encString}]);
+};
 
-	var argscheck = require('cordova/argscheck'),
-	    channel = require('cordova/channel'),
-	    utils = require('cordova/utils'),
-	    exec = require('cordova/exec'),
-	    cordova = require('cordova');
-	
-	Crypto.prototype.encrypt = function(successCallback, errorCallback, params) {
-		var plainText 	= params.text || '';
-    	var returnTo 	= params.returnTo || function(){};
-	    exec(successCallback, errorCallback, "Crypto", "encrypt", [{data: plainText}]);
-	};
-	
-	Crypto.prototype.decrypt = function(successCallback, errorCallback) {
-	    exec(successCallback, errorCallback, "Crypto", "decrypt", [{data: encString}]);
-	};
-	
-	module.exports = new Crypto();
+Crypto.prototype.test = function() {
+	return "test complete";
+};
+
+module.exports = new Crypto();
