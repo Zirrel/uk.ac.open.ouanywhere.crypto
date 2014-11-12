32f4c648db20a9cf9ea9139e453865ce51ed1cc5
Mark Steele <zirrelstudios@gmail.com>
2014-11-12 13:58:10 +0000

    uhiu

--- www/crypto.js
+++ www/crypto.js
@@ -6,12 +6,14 @@ cordova.define("uk.ac.open.ouanywhere.crypto", function(require, exports, module
 	    exec = require('cordova/exec'),
 	    cordova = require('cordova');
 	
-	Crypto.prototype.encrypt = function(successCallback, errorCallback) {
-	    exec(successCallback, errorCallback, "Crypto", "encrypt", []);
+	Crypto.prototype.encrypt = function(successCallback, errorCallback, params) {
+		var plainText 	= params.text || '';
+    	var returnTo 	= params.returnTo || function(){};
+	    exec(successCallback, errorCallback, "Crypto", "encrypt", [{data: plainText}]);
 	};
 	
 	Crypto.prototype.decrypt = function(successCallback, errorCallback) {
-	    exec(successCallback, errorCallback, "Crypto", "decrypt", []);
+	    exec(successCallback, errorCallback, "Crypto", "decrypt", [{data: encString}]);
 	};
 	
 	module.exports = new Crypto();
