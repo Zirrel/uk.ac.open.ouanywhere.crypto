728225a440dfb17f5d77b0e5eed6f432dda46174
Mark Steele <zirrelstudios@gmail.com>
2014-11-26 00:32:24 +0000

    ytfy

--- src/ios/CDVCrypto.h
+++ src/ios/CDVCrypto.h
@@ -2,15 +2,14 @@
 //  CDVCrypto.h
 //  OUAnywhere
 //
-//  Created by Paul.Hogan on 09/07/2013.
-//  Copyright (c) 2013 The Open University. All rights reserved.
-//
 
 #import <Cordova/CDV.h>
 
 @interface CDVCrypto : CDVPlugin
 
--(void)encrypt:(NSMutableArray *)arguments withDict:(NSMutableDictionary*)options;
--(void)decrypt:(NSMutableArray *)arguments withDict:(NSMutableDictionary*)options;
+-(void)encrypt:(CDVInvokedUrlCommand *)command;
+-(void)decrypt:(CDVInvokedUrlCommand *)command;
+
+-(void)test:(CDVInvokedUrlCommand *)command;
 
 @end
--- src/ios/CDVCrypto.m
+++ src/ios/CDVCrypto.m
@@ -2,50 +2,48 @@
 //  CDVCrypto.m
 //  OUAnywhere
 //
-//  Created by Paul.Hogan on 09/07/2013.
-//  Copyright (c) 2013 The Open University. All rights reserved.
-//
 
 #import "CDVCrypto.h"
 #import "RNEncryptor.h"
 #import "RNDecryptor.h"
 
 @implementation CDVCrypto {
-    
     CDVPluginResult *pluginResult;
-    
     NSString *callbackID;
 }
 
--(void)encrypt:(NSMutableArray *)arguments withDict:(NSMutableDictionary*)options {
+-(void)test:(CDVInvokedUrlCommand *)command {
+    callbackID = command.callbackId;
     
-    callbackID = [arguments objectAtIndex:0];
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Crypto"];
     
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+-(void)encrypt:(CDVInvokedUrlCommand *)command {
+    callbackID = command.callbackId;
+    
+    NSMutableDictionary* options = [command.arguments objectAtIndex:0];
     NSError *error = nil;
-    
+
     NSData *data = [RNEncryptor encryptData:[[options objectForKey:@"data"] dataUsingEncoding:NSUTF8StringEncoding]
                                withSettings:kRNCryptorAES256Settings
                                    password:[NSString stringWithFormat:@"%@%@",[[NSBundle mainBundle] bundleIdentifier], [[NSBundle mainBundle] bundlePath]]
                                       error:&error];
 
     if (error) {
-        
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"encryption error"];
-        
-        [self writeJavascript: [pluginResult toErrorCallbackString:callbackID]];
-    }
-    else {
-        
+    } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[data base64EncodedString]];
-        
-        [self writeJavascript: [pluginResult toSuccessCallbackString:callbackID]];        
     }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
--(void)decrypt:(NSMutableArray *)arguments withDict:(NSMutableDictionary*)options {
-   
-    callbackID = [arguments objectAtIndex:0];
+-(void)decrypt:(CDVInvokedUrlCommand *)command {
+    callbackID = command.callbackId;
     
+    NSMutableDictionary* options = [command.arguments objectAtIndex:0];
     NSError *error = nil;
     
     NSData *data = [RNDecryptor decryptData:[NSData dataFromBase64String:[options objectForKey:@"data"]]
@@ -53,17 +51,12 @@
                                       error:&error];
     
     if (error) {
-        
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"decryption error"];
-        
-        [self writeJavascript: [pluginResult toErrorCallbackString:callbackID]];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[[NSString alloc] initWithUTF8String:[data bytes]]];
     }
-    else {
-        
-         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[[NSString alloc] initWithUTF8String:[data bytes]]];
-        
-        [self writeJavascript: [pluginResult toSuccessCallbackString:callbackID]];
-    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 @end
